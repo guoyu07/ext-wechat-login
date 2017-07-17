085f544886bb9329002eb11d987c72f1ef4775ef
谷祖楠 <13793165851@163.com>
2017-07-17 15:27:48 +0800

    添加设置和获取参数路由

--- src/Listeners/RouteRegister.php
+++ src/Listeners/RouteRegister.php
@@ -11,9 +11,9 @@
 namespace Notadd\WechatLogin\Listeners;
 
 
-use Notadd\WechatLogin\Controllers\TestController;
-
 use Notadd\Foundation\Routing\Abstracts\RouteRegister as AbstractRouteRegister;
+use Notadd\WechatLogin\Controllers\WechatOpenController;
+
 /**
  * Class RouteRegister.
  */
@@ -24,10 +24,17 @@ class RouteRegister extends AbstractRouteRegister
      */
     public function handle()
     {
-        $this->router->group(['middleware' => ['cross', 'web'], 'prefix' => 'api/wechat'], function () {
-            $this->router->any('test', TestController::class . '@test');
+        $this->router->any('returnback', WechatOpenController::class . '@returnUrl');
 
-            $this->router->any('callback', TestController::class . '@callback');
+        $this->router->group(['middleware' => ['cross', 'web'], 'prefix' => 'api/wechat'], function () {
+
+            $this->router->any('test', WechatOpenController::class . '@test');
+
+            $this->router->any('callback', WechatOpenController::class . '@callback');
+
+            $this->router->post('set', WechatOpenController::class . '@set');
+
+            $this->router->post('get', WechatOpenController::class . '@get');
         });
     }
 }
