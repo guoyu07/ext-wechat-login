2105e7b8cc5e9068ef0d6ebaabe661ed92b4b3e1
谷祖楠 <13793165851@163.com>
2017-07-18 16:52:26 +0800

    添加后台微信二维码登陆

---
+++ src/Handlers/BackAuthHandler.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The file is part of Notadd
+ *
+ * @author: AllenGu<674397601@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime: 17-7-18 下午3:51
+ */
+
+namespace Notadd\WechatLogin\Handlers;
+
+
+class BackAuthHandler
+{
+
+}
