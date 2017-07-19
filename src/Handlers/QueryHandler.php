fc74cc3bfdddfcc6d751354fb8a335a1db7cc72e
谷祖楠 <13793165851@163.com>
2017-07-19 10:24:43 +0800

    添加轮询接口

---
+++ src/Handlers/QueryHandler.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The file is part of Notadd
+ *
+ * @author: AllenGu<674397601@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime: 17-7-18 下午5:33
+ */
+
+namespace Notadd\WechatLogin\Handlers;
+
+
+class QueryHandler
+{
+
+}
